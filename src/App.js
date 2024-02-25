e5d4570f3867af4e9075189105b34de81dd0c4d2
esmailessameldin <76599624+esmailessameldin@users.noreply.github.com>
2024-02-25 17:05:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import {TextInput,RadioButton,RadioButtonGroup,Button,Stack,FormGroup} from '@carbon/react'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <FormGroup style={{
+      maxWidth: '400px'
+    }} legendText="FormGroup Legend">
+        <Stack gap={7}>
+          <TextInput data-testid="first name" id="one" labelText="First Name" />
+          <TextInput data-testid="last name" labelText="Last Name" />
+          <RadioButtonGroup data-testid="RadioButtonGroup" legendText="Radio button heading" name="radio-button-group" defaultSelected="radio-1">
+            <RadioButton data-testid="RadioButton Option 1" labelText="Option 1" value="radio-1" id="radio-1" />
+            <RadioButton data-testid="RadioButton Option 2"labelText="Option 2" value="radio-2" id="radio-2" />
+            <RadioButton data-testid="RadioButton Option 3" labelText="Option 3" value="radio-3" id="radio-3" />
+          </RadioButtonGroup>
+          <Button data-testid="Submit Button">Submit</Button>
+        </Stack>
+      </FormGroup>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen ,fireEvent} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test("Component  should be rendered", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const usernameInputEl = screen.getByTestId(/first name/i);
+  expect(usernameInputEl).toBeInTheDocument();
+   const  lastname=screen.getByTestId(/last name/i)
+   expect(lastname).toBeInTheDocument();
+   expect(screen.getByTestId(/RadioButtonGroup/i)).toBeInTheDocument();
+   expect(screen.getByTestId(/RadioButton Option 1/i)).toBeInTheDocument();
+   expect(screen.getByTestId(/RadioButton Option 2/i)).toBeInTheDocument();
+   expect(screen.getByTestId(/RadioButton Option 3/i)).toBeInTheDocument();
+   expect(screen.getByTestId(/Submit Button/i)).toBeInTheDocument();
+   
+});
+
+test("text space empty",()=>{
+  render(<App></App>);
+  expect(screen.getByTestId(/first name/i).value).toBe("");
+  expect(screen.getByTestId(/last name/i).value).toBe("");
+
+
+})
+test("Radio tests",()=>{
+  render(<App></App>);
+  const radioButton1 = screen.getByTestId(/RadioButton Option 1/i);
+  const radioButton2 = screen.getByTestId(/RadioButton Option 2/i);
+  const radioButton3 = screen.getByTestId(/RadioButton Option 3/i);
+ 
+  expect(radioButton1.defaultChecked).toBe(true)
+  expect(radioButton2.defaultChecked).toBe(false)
+  
+ 
+  //expect(screen.getByLabelText(/option 2/i)).toBe("")
+
+
+})
+test("Radio tests",()=>{
+  render(<App></App>);
+  const button=screen.getByTestId(/Submit Button/i)
+expect(button.textContent).toBe("Submit")
+
+})
+test("name input should change", () => {
+  render(<App />);
+  const firstname = screen.getByTestId(/first name/i);
+  const testValue = "test";
+  
+  fireEvent.change(firstname, { target: { value: testValue } });
+  expect(firstname.value).toBe("test");
+
+  fireEvent.change(screen.getByTestId(/last name/i),{target:{value:"test"}})
+  expect(screen.getByTestId(/last name/i).value).toBe("test")
+
 });
